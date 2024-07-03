a6009f19d61fc5d54b7a81ecb9649af69a3442a1
Artyom Pizin <artempetr2016@gmail.com>
2024-07-03 19:47:43 +0300

    chore: extend eslint with plugins, rules and additional options

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,70 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
+  'root': true,
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
     'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
+  'ignorePatterns': ['dist', '.eslintrc.cjs'],
+  'overrides': [
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    'react-refresh',
+    'react-compiler',
+    '@typescript-eslint',
+    'import',
+    'jsx-a11y'
+  ],
+  'settings': {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
+    },
+    "import/resolver": {
+      "typescript": {
+        "alwaysTryTypes": true,
+      }
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+  'rules': {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-  },
-}
+    'react-compiler/react-compiler': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    "import/no-unresolved": "error",
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+};
