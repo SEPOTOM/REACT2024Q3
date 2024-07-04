5eb38cd57261675dae12ccb00474d846c4c3a811
Artyom Pizin <artempetr2016@gmail.com>
2024-07-04 13:19:31 +0300

    style: format code using Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,10 @@
 module.exports = {
-  'root': true,
-  'env': {
-    'browser': true,
-    'es2021': true
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
+  extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
@@ -12,45 +12,44 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:import/recommended',
     'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
-  'ignorePatterns': ['dist', '.eslintrc.cjs'],
-  'overrides': [
-  ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  'plugins': [
+  plugins: [
     'react',
     'react-refresh',
     'react-compiler',
     '@typescript-eslint',
     'import',
     'jsx-a11y',
-    'prettier'
+    'prettier',
   ],
-  'settings': {
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"]
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
-    "import/resolver": {
-      "typescript": {
-        "alwaysTryTypes": true,
-      }
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
     },
     react: {
       version: 'detect',
     },
   },
-  'rules': {
+  rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
     'react-compiler/react-compiler': 'error',
     '@typescript-eslint/no-explicit-any': 'error',
-    "import/no-unresolved": "error"
-  }
+    'import/no-unresolved': 'error',
+  },
 };
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,4 +1,4 @@
 module.exports = {
   experimentalTernaries: true,
   singleQuote: true,
-}
+};
