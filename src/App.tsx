1f2c4b9c2815b4567e0b3f9b3d0321cf28ead1c8
Artyom Pizin <artempetr2016@gmail.com>
2024-07-05 21:44:26 +0300

    refactor: rewrite App component as class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-function App() {
-  return <div>App</div>;
+import { Component, ReactNode } from 'react';
+
+class App extends Component {
+  render(): ReactNode {
+    return <div>App</div>;
+  }
 }
 
 export default App;
