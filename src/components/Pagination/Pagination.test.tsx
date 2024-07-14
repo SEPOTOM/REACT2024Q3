6c2762d7c6b2269499ed47ebf613b160e9e6702c
Artyom Pizin <artempetr2016@gmail.com>
2024-07-14 22:14:10 +0300

    test: update Pagination tests to match new Pagination props

--- src/components/Pagination/Pagination.test.tsx
+++ src/components/Pagination/Pagination.test.tsx
@@ -8,7 +8,7 @@ test('Pagination updates URL query parameter when page changes', async () => {
   window.history.pushState(null, '', '/search/1');
   const { user, getByRole } = renderWithUser(
     <BrowserRouter>
-      <Pagination />
+      <Pagination totalPages={2} />
     </BrowserRouter>,
   );
 
