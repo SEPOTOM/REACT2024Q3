323064a3ed1500596dd6ce5f4af0b69fda6bbdce
Artyom Pizin <artempetr2016@gmail.com>
2024-07-14 19:09:15 +0300

    feat: add ErrorPage component and use it as errorElement for the router

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,24 +1,29 @@
 import { Navigate, RouteObject } from 'react-router-dom';
 
-import { MainPage, NotFoundPage, ProductPage } from '@/views';
+import { ErrorPage, MainPage, NotFoundPage, ProductPage } from '@/views';
 
 export const routes: RouteObject[] = [
   {
-    path: '/',
-    element: <Navigate to="/search/1" replace />,
-  },
-  {
-    path: '/search/:searchPage',
-    element: <MainPage />,
+    errorElement: <ErrorPage />,
     children: [
       {
-        path: 'details',
-        element: <ProductPage />,
+        path: '/',
+        element: <Navigate to="/search/1" replace />,
+      },
+      {
+        path: '/search/:searchPage',
+        element: <MainPage />,
+        children: [
+          {
+            path: 'details',
+            element: <ProductPage />,
+          },
+        ],
+      },
+      {
+        path: '*',
+        element: <NotFoundPage />,
       },
     ],
-  },
-  {
-    path: '*',
-    element: <NotFoundPage />,
   },
 ];
--- src/views/index.ts
+++ src/views/index.ts
@@ -1,5 +1,6 @@
 import MainPage from '@views/MainPage/MainPage';
 import NotFoundPage from '@views/NotFoundPage/NotFoundPage';
 import ProductPage from '@views/ProductPage/ProductPage';
+import ErrorPage from '@views/ErrorPage/ErrorPage';
 
-export { MainPage, NotFoundPage, ProductPage };
+export { MainPage, NotFoundPage, ProductPage, ErrorPage };
