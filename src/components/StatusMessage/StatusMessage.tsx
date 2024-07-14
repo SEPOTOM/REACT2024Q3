24abe01d89105f5eaf65c243ebed3e8f17d856d7
Artyom Pizin <artempetr2016@gmail.com>
2024-07-14 15:44:35 +0300

    feat: add role status to the StatusMessage

--- src/components/StatusMessage/StatusMessage.tsx
+++ src/components/StatusMessage/StatusMessage.tsx
@@ -3,7 +3,11 @@ import { StatusMessageProps } from '@components/StatusMessage/types';
 import '@components/StatusMessage/StatusMessage.css';
 
 const StatusMessage = ({ children }: StatusMessageProps) => {
-  return <p className="status-message">{children}</p>;
+  return (
+    <p role="status" className="status-message">
+      {children}
+    </p>
+  );
 };
 
 export default StatusMessage;
