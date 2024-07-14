1b106a126bf3f894e29ee7214bd87a9a42856e2d
Artyom Pizin <artempetr2016@gmail.com>
2024-07-14 20:16:43 +0300

    feat: update NotFoundPage styles

--- src/views/NotFoundPage/NotFoundPage.tsx
+++ src/views/NotFoundPage/NotFoundPage.tsx
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom';
 
+import '@views/NotFoundPage/NotFoundPage.css';
+
 const NotFoundPage = () => {
   return (
-    <div>
-      <span>404</span>
-      <h2>The requested page was not found</h2>
-      <Link to="/search/1">To the main page</Link>
+    <div className="not-found-page">
+      <span className="not-found-page__status">404</span>
+      <h2 className="not-found-page__title">
+        The requested page was not found
+      </h2>
+      <Link to="/search/1" className="not-found-page__link">
+        To the main page
+      </Link>
     </div>
   );
 };
