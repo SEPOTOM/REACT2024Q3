45d5ea5c840efc2b2f19b7ef63dc2c48dce6265b
Artyom Pizin <artempetr2016@gmail.com>
2024-07-14 14:23:55 +0300

    feat: update MainPage to accept child routes layouts

--- src/views/MainPage/MainPage.tsx
+++ src/views/MainPage/MainPage.tsx
@@ -1,3 +1,5 @@
+import { useOutlet } from 'react-router-dom';
+
 import {
   ErrorButton,
   Pagination,
@@ -13,34 +15,38 @@ import '@views/MainPage/MainPage.css';
 const MainPage = () => {
   const [searchQuery, setSearchQuery] = useSearchQuery();
   const products = useProducts(searchQuery);
+  const outlet = useOutlet();
 
   const handleSearchFormSubmit = (newSearchQuery: string): void => {
     setSearchQuery(newSearchQuery);
   };
 
   return (
-    <>
-      <header className="header">
-        <div className="container header__inner">
-          <ErrorButton />
-          <SearchForm
-            initialSearchQuery={searchQuery}
-            onFormSubmit={handleSearchFormSubmit}
-          />
-        </div>
-      </header>
-      <main className="main">
-        <div className="container main__inner">
-          {products && products.length > 0 && (
-            <>
-              <ProductsList products={products} />
-              <Pagination />
-            </>
-          )}
-          {products === null && <StatusMessage>Loading...</StatusMessage>}
-        </div>
-      </main>
-    </>
+    <div className="main-page">
+      <div className="main-page__column">
+        <header className="header">
+          <div className="container header__inner">
+            <ErrorButton />
+            <SearchForm
+              initialSearchQuery={searchQuery}
+              onFormSubmit={handleSearchFormSubmit}
+            />
+          </div>
+        </header>
+        <main className="main">
+          <div className="container main__inner">
+            {products && products.length > 0 && (
+              <>
+                <ProductsList products={products} />
+                <Pagination />
+              </>
+            )}
+            {products === null && <StatusMessage>Loading...</StatusMessage>}
+          </div>
+        </main>
+      </div>
+      {outlet && <div className="main-page__column">{outlet}</div>}
+    </div>
   );
 };
 
