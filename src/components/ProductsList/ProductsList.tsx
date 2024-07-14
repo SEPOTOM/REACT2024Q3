cf0d2a133dc72f3d7566cc21bdc08dad67d9eb95
Artyom Pizin <artempetr2016@gmail.com>
2024-07-14 09:40:03 +0300

    refactor: rewrite ProductsList component as a functional one

--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -1,22 +1,18 @@
-import { Component, ReactNode } from 'react';
-
 import { ProductsListProps } from '@components/ProductsList/types';
 
 import '@components/ProductsList/ProductsList.css';
 
-class ProductsList extends Component<ProductsListProps> {
-  render(): ReactNode {
-    return (
-      <ul className="products-list">
-        {this.props.products.map((product) => (
-          <li key={product.id} className="products-list__item">
-            <h2 className="products-list__title">{product.title}</h2>
-            <p className="products-list__text">{product.description}</p>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-}
+const ProductsList = ({ products }: ProductsListProps) => {
+  return (
+    <ul className="products-list">
+      {products.map((product) => (
+        <li key={product.id} className="products-list__item">
+          <h2 className="products-list__title">{product.title}</h2>
+          <p className="products-list__text">{product.description}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default ProductsList;
