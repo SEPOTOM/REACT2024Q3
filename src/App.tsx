8b8a110fa81bfe3b1bdb79f1c0cb4c57a098e883
Artyom Pizin <artempetr2016@gmail.com>
2024-07-14 13:01:27 +0300

    feat: add router with root and search routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
+
 import { MainPage } from '@/views';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate to="/search/1" replace />,
+  },
+  {
+    path: '/search/:searchPage',
+    element: <MainPage />,
+  },
+]);
+
 const App = () => {
-  return <MainPage />;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
