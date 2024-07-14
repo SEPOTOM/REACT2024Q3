86c10806a59b8a753b7fc40e5bf337fa57bee76b
Artyom Pizin <artempetr2016@gmail.com>
2024-07-14 18:01:07 +0300

    test: mock services for MainPage tests

--- src/views/MainPage/MainPage.test.tsx
+++ src/views/MainPage/MainPage.test.tsx
@@ -1,12 +1,39 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { waitFor } from '@testing-library/react';
+
 import { renderWithUser } from '@tests/utils';
 
 import { getSearchQuery, saveSearchQuery } from '@utils/localStorage';
 
-import { MainPage } from '@/views';
+import { routes } from '@/routes';
+
+vi.mock('@services/api', () => {
+  return {
+    getProductsBySearchQuery: vi.fn().mockImplementation(async () => {
+      return [
+        { title: 'Product 1', description: 'Description 1', id: 1 },
+        { title: 'Product 2', description: 'Description 2', id: 2 },
+      ];
+    }),
+    getProductById: vi.fn().mockImplementation(async (productId: number) => {
+      return {
+        title: `Detailed Product ${productId}`,
+        description: `Detailed Description ${productId}`,
+        category: `Detailed Category ${productId}`,
+        price: 9.99,
+        images: [`Detailed Image ${productId}`],
+        id: productId,
+      };
+    }),
+  };
+});
 
 test('MainPage saves the entered search query to the local storage when the Search button is clicked', async () => {
   localStorage.clear();
-  const { user, getByRole } = renderWithUser(<MainPage />);
+  const router = createBrowserRouter(routes);
+  const { user, getByRole } = renderWithUser(
+    <RouterProvider router={router} />,
+  );
 
   await user.type(getByRole('searchbox'), 'Test query');
   await user.click(getByRole('button', { name: /search/i }));
@@ -14,11 +41,14 @@ test('MainPage saves the entered search query to the local storage when the Sear
   expect(getSearchQuery()).toBe('Test query');
 });
 
-test('MainPage retrieves the search query from the local storage upon mounting', () => {
+test('MainPage retrieves the search query from the local storage upon mounting', async () => {
   localStorage.clear();
   saveSearchQuery('Saved search query');
+  const router = createBrowserRouter(routes);
 
-  const { getByRole } = renderWithUser(<MainPage />);
+  const { getByRole } = renderWithUser(<RouterProvider router={router} />);
 
-  expect(getByRole('searchbox')).toHaveDisplayValue('Saved search query');
+  await waitFor(() => {
+    expect(getByRole('searchbox')).toHaveDisplayValue('Saved search query');
+  });
 });
