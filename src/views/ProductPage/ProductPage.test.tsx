13a630976f72f0da61351d895f5f0775212e4967
Artyom Pizin <artempetr2016@gmail.com>
2024-07-14 18:37:21 +0300

    test: add test for ProductPage displaying detailed product data

--- src/views/ProductPage/ProductPage.test.tsx
+++ src/views/ProductPage/ProductPage.test.tsx
@@ -35,3 +35,21 @@ test('ProductPage displays a loading indicator while fetching data', async () =>
 
   expect(await findByRole('status')).toHaveTextContent(/loading/i);
 });
+
+test('ProductPage correctly displays the detailed product data', async () => {
+  window.history.pushState(null, '', '/search/1/details?product=1');
+
+  const { findByAltText, getByRole, getByText } = renderWithUser(
+    <BrowserRouter>
+      <ProductPage />
+    </BrowserRouter>,
+  );
+
+  expect(await findByAltText(/Detailed Product 1/i)).toBeInTheDocument();
+  expect(
+    getByRole('heading', { name: /Detailed Product 1/i }),
+  ).toBeInTheDocument();
+  expect(getByText(/Detailed Description 1/i)).toBeInTheDocument();
+  expect(getByText(/Detailed Category 1/i)).toBeInTheDocument();
+  expect(getByText(/9.99/i)).toBeInTheDocument();
+});
