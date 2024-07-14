d18a475c2121056547da94eb03b9574853c2956f
Artyom Pizin <artempetr2016@gmail.com>
2024-07-14 13:09:32 +0300

    test: add test for retrieving search query from local storage

--- src/views/MainPage/MainPage.test.tsx
+++ src/views/MainPage/MainPage.test.tsx
@@ -1,6 +1,6 @@
 import { renderWithUser } from '@tests/utils';
 
-import { getSearchQuery } from '@utils/localStorage';
+import { getSearchQuery, saveSearchQuery } from '@utils/localStorage';
 
 import { MainPage } from '@/views';
 
@@ -12,4 +12,13 @@ test('MainPage saves the entered search query to the local storage when the Sear
   await user.click(getByRole('button', { name: /search/i }));
 
   expect(getSearchQuery()).toBe('Test query');
+});
+
+test('MainPage retrieves the search query from the local storage upon mounting', () => {
+  localStorage.clear();
+  saveSearchQuery('Saved search query');
+
+  const { getByRole } = renderWithUser(<MainPage />);
+
+  expect(getByRole('searchbox')).toHaveDisplayValue('Saved search query');
 });
