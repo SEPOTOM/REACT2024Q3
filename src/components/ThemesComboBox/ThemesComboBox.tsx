afe88518c84bd032a0f4d77ae9968694c0a70aea
Artyom Pizin <artempetr2016@gmail.com>
2024-07-18 14:32:47 +0300

    feat(ThemesComboBox): implement theme change functionality

--- src/components/ThemesComboBox/ThemesComboBox.tsx
+++ src/components/ThemesComboBox/ThemesComboBox.tsx
@@ -1,4 +1,6 @@
-import { useId } from 'react';
+import { ChangeEvent, useId } from 'react';
+
+import { useTheme, useUpdateTheme } from '@/contexts';
 
 import { capitalize } from '@/utils/strings';
 
@@ -8,11 +10,17 @@ import '@components/ThemesComboBox/ThemesComboBox.css';
 
 const ThemesComboBox = () => {
   const themeSelectId = useId();
+  const theme = useTheme();
+  const updateTheme = useUpdateTheme();
+
+  const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    updateTheme(e.target.value);
+  };
 
   return (
     <div>
       <label htmlFor={themeSelectId}>Theme:</label>
-      <select id={themeSelectId}>
+      <select id={themeSelectId} value={theme} onChange={handleSelectChange}>
         {Object.values(Themes).map((theme) => {
           return (
             <option value={theme} key={theme}>
