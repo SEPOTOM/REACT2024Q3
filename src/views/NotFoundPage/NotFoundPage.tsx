4115ff4e47a4f62b0df625beb4a021b9ca40dee4
Artyom Pizin <artempetr2016@gmail.com>
2024-07-18 16:44:23 +0300

    feat(NotFoundPage): add dark theme support and styles

--- src/views/NotFoundPage/NotFoundPage.tsx
+++ src/views/NotFoundPage/NotFoundPage.tsx
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom';
 
+import { useTheme } from '@/contexts';
+
 import '@views/NotFoundPage/NotFoundPage.css';
 
 const NotFoundPage = () => {
+  const theme = useTheme();
+
   return (
-    <div className="not-found-page">
+    <div className={`not-found-page not-found-page_theme_${theme}`}>
       <span className="not-found-page__status">404</span>
       <h2 className="not-found-page__title">
         The requested page was not found
