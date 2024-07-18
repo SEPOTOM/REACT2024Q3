19f2c9536ff67fdd66a1a8dab698d686619a989b
Artyom Pizin <artempetr2016@gmail.com>
2024-07-18 16:44:01 +0300

    feat(MainPage): add dark theme support and styles

--- src/views/MainPage/MainPage.tsx
+++ src/views/MainPage/MainPage.tsx
@@ -9,6 +9,8 @@ import {
   ThemesComboBox,
 } from '@/components';
 
+import { useTheme } from '@/contexts';
+
 import { useProducts, useSearchQuery } from '@/hooks';
 
 import '@views/MainPage/MainPage.css';
@@ -17,13 +19,14 @@ const MainPage = () => {
   const [searchQuery, setSearchQuery] = useSearchQuery();
   const products = useProducts(searchQuery);
   const outlet = useOutlet();
+  const theme = useTheme();
 
   const handleSearchFormSubmit = (newSearchQuery: string): void => {
     setSearchQuery(newSearchQuery);
   };
 
   return (
-    <div className="main-page">
+    <div className={`main-page main-page_theme_${theme}`}>
       <div className="main-page__column">
         <header className="header">
           <div className="container header__inner">
