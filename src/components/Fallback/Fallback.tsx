52260bf1082a1980e8be420847b6551e5cb01def
Artyom Pizin <artempetr2016@gmail.com>
2024-07-18 16:41:35 +0300

    feat(Fallback): add dark theme support and styles

--- src/components/Fallback/Fallback.tsx
+++ src/components/Fallback/Fallback.tsx
@@ -1,10 +1,14 @@
+import { useTheme } from '@/contexts';
+
 import { FallbackProps } from '@components/Fallback/types';
 
 import '@components/Fallback/Fallback.css';
 
 const Fallback = ({ error }: FallbackProps) => {
+  const theme = useTheme();
+
   return (
-    <div className="fallback">
+    <div className={`fallback fallback_theme_${theme}`}>
       <h2 className="fallback__title">An unexpected error has occurred!</h2>
       <p className="fallback__text">
         Error: {error instanceof Error ? error.message : String(error)}
