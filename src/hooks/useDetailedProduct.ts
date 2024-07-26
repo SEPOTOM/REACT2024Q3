fb38337e3a3523c8b90636adf5e8ed30d4d49c0f
Artyom Pizin <artempetr2016@gmail.com>
2024-07-26 10:01:41 +0300

    feat(useDetailedProduct): use useGetProductByIdQuery instead of getProductById

--- src/hooks/useDetailedProduct.ts
+++ src/hooks/useDetailedProduct.ts
@@ -1,38 +1,18 @@
-import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
-import { getProductById } from '@services/api';
+import { useGetProductByIdQuery } from '@/store/api/apiSlice';
+
 import { DetailedProduct } from '@services/types';
 
 const useDetailedProduct = (): DetailedProduct | null => {
-  const [detailedProduct, setDetailedProduct] =
-    useState<DetailedProduct | null>(null);
   const [searchParams] = useSearchParams();
 
   const productSearchParam = searchParams.get('product');
   const productId = productSearchParam ? Number(productSearchParam) : 1;
 
-  useEffect(() => {
-    let isMounted = true;
+  const { data: detailedProduct } = useGetProductByIdQuery(productId);
 
-    const fetchDetailedProduct = async (): Promise<void> => {
-      setDetailedProduct(null);
-
-      const newProducts = await getProductById(productId);
-
-      if (isMounted) {
-        setDetailedProduct(newProducts);
-      }
-    };
-
-    fetchDetailedProduct();
-
-    return () => {
-      isMounted = false;
-    };
-  }, [productId]);
-
-  return detailedProduct;
+  return detailedProduct ? detailedProduct : null;
 };
 
 export default useDetailedProduct;
