ea2f06d0a8f5e40b73c90ac4e948081bf9c80051
Artyom Pizin <artempetr2016@gmail.com>
2024-07-26 09:45:48 +0300

    feat(apiSlice): add getProducts query endpoint

--- src/store/api/apiSlice.ts
+++ src/store/api/apiSlice.ts
@@ -1,7 +1,34 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+import { SearchProductsResponse } from '@services/types';
+import { GetProductsParams } from '@store/api/types';
+
+import { PRODUCTS_PER_PAGE_AMOUNT } from '@/consts';
+
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://dummyjson.com' }),
-  endpoints: () => ({}),
+  endpoints: (builder) => ({
+    getProducts: builder.query<SearchProductsResponse, GetProductsParams>({
+      query: ({ searchQuery, page = 1 }) => {
+        let pathname = '/products';
+        const searchParams = new URLSearchParams({
+          limit: String(PRODUCTS_PER_PAGE_AMOUNT),
+          select: 'title,description',
+          skip: String((page - 1) * PRODUCTS_PER_PAGE_AMOUNT),
+        });
+
+        if (searchQuery) {
+          pathname += '/search';
+          searchParams.append('q', searchQuery);
+        }
+
+        pathname += `?${searchParams.toString()}`;
+
+        return pathname;
+      },
+    }),
+  }),
 });
+
+export const { useGetProductsQuery } = apiSlice;
---
+++ src/store/api/types.ts
@@ -0,0 +1,4 @@
+export interface GetProductsParams {
+  searchQuery?: string;
+  page?: number;
+}
