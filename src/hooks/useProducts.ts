1e3b8e7539e963eb33f407c6dc72d2fa4b6c969e
Artyom Pizin <artempetr2016@gmail.com>
2024-07-26 09:46:58 +0300

    feat(useProducts): use useGetProductsQuery instead of getProductsBySearchQuery

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,8 +1,6 @@
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { getProductsBySearchQuery } from '@services/api';
-import { SearchProductsResponse } from '@services/types';
+import { useGetProductsQuery } from '@/store/api/apiSlice';
 
 import { validatePage } from '@/utils/validation';
 
@@ -11,34 +9,13 @@ import { ProductsData } from '@hooks/types';
 import { PRODUCTS_PER_PAGE_AMOUNT } from '@/consts';
 
 const useProducts = (searchQuery: string): ProductsData | null => {
-  const [productsResponse, setProductsResponse] =
-    useState<SearchProductsResponse | null>(null);
   const { searchPage } = useParams();
-
   const currentPage = validatePage(searchPage);
 
-  useEffect(() => {
-    let isMounted = true;
-
-    const fetchProducts = async (): Promise<void> => {
-      setProductsResponse(null);
-
-      const newProductsResponse = await getProductsBySearchQuery(
-        searchQuery.trim(),
-        currentPage,
-      );
-
-      if (isMounted) {
-        setProductsResponse(newProductsResponse);
-      }
-    };
-
-    fetchProducts();
-
-    return () => {
-      isMounted = false;
-    };
-  }, [searchQuery, currentPage]);
+  const { data: productsResponse } = useGetProductsQuery({
+    searchQuery,
+    page: currentPage,
+  });
 
   const totalPages = Math.floor(
     productsResponse ? productsResponse.total / PRODUCTS_PER_PAGE_AMOUNT : 0,
