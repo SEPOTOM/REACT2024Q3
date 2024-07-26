5147cc763ad928987a976a25ed887efc3bd4954c
Artyom Pizin <artempetr2016@gmail.com>
2024-07-26 12:32:39 +0300

    test: add handler for mocking get posts response

--- src/__tests__/mocks/handlers.ts
+++ src/__tests__/mocks/handlers.ts
@@ -1,3 +1,13 @@
-import { HttpHandler } from 'msw';
+import { HttpHandler, HttpResponse, http } from 'msw';
 
-export const handlers: HttpHandler[] = [];
+export const handlers: HttpHandler[] = [
+  http.get(new RegExp('^https://dummyjson.com/products(/search)?$'), () => {
+    return HttpResponse.json({
+      total: 2,
+      products: [
+        { title: 'Product 1', description: 'Description 1', id: 1 },
+        { title: 'Product 2', description: 'Description 2', id: 2 },
+      ],
+    });
+  }),
+];
