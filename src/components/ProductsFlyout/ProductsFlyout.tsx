037c59db663489531791d0269f57b85c2552245c
Artyom Pizin <artempetr2016@gmail.com>
2024-07-26 18:18:39 +0300

    feat(ProductsFlyout): implement functionality to delete checked products

--- src/components/ProductsFlyout/ProductsFlyout.tsx
+++ src/components/ProductsFlyout/ProductsFlyout.tsx
@@ -1,6 +1,9 @@
-import { selectCheckedProducts } from '@store/checkedProducts/checkedProductsSlice';
+import {
+  checkedProductsDeleted,
+  selectCheckedProducts,
+} from '@store/checkedProducts/checkedProductsSlice';
 
-import { useAppSelector } from '@store/hooks';
+import { useAppDispatch, useAppSelector } from '@store/hooks';
 import { useTheme } from '@/contexts';
 
 import '@components/ProductsFlyout/ProductsFlyout.css';
@@ -8,6 +11,11 @@ import '@components/ProductsFlyout/ProductsFlyout.css';
 const ProductsFlyout = () => {
   const theme = useTheme();
   const checkedProducts = useAppSelector(selectCheckedProducts);
+  const dispatch = useAppDispatch();
+
+  const handleUnselectAllClick = () => {
+    dispatch(checkedProductsDeleted());
+  };
 
   return checkedProducts.length > 0 ?
       <div
@@ -19,6 +27,13 @@ const ProductsFlyout = () => {
             {checkedProducts.length}{' '}
             {checkedProducts.length > 1 ? 'products' : 'product'} selected
           </p>
+          <button
+            type="button"
+            onClick={handleUnselectAllClick}
+            className="products-flyout__button"
+          >
+            Unselect all
+          </button>
         </div>
       </div>
     : null;
