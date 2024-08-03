f4a1a36b69e6c1f660a7fb74d3a8064264f4976b
Artyom Pizin <artempetr2016@gmail.com>
2024-08-03 15:51:02 +0300

    refactor: remove react-router-dom and corresponding unnecessary files

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,6 +1,5 @@
 import MainPage from '@views/MainPage/MainPage';
 import NotFoundPage from '@views/NotFoundPage/NotFoundPage';
 import ProductPage from '@views/ProductPage/ProductPage';
-import ErrorPage from '@views/ErrorPage/ErrorPage';
 
-export { MainPage, NotFoundPage, ProductPage, ErrorPage };
+export { MainPage, NotFoundPage, ProductPage };
