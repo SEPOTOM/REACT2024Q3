6520076f51eb2e61a2d7cbc6cc37c00b7de1d955
Artyom Pizin <artempetr2016@gmail.com>
2024-08-05 17:33:30 +0300

    feat: add useIsPageLoading hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,5 +2,12 @@ import useSearchQuery from '@/hooks/useSearchQuery';
 import useProducts from '@/hooks/useProducts';
 import useDetailedProduct from '@/hooks/useDetailedProduct';
 import useCurrentPage from '@/hooks/useCurrentPage';
+import useIsPageLoading from '@/hooks/useIsPageLoading';
 
-export { useSearchQuery, useProducts, useDetailedProduct, useCurrentPage };
+export {
+  useSearchQuery,
+  useProducts,
+  useDetailedProduct,
+  useCurrentPage,
+  useIsPageLoading,
+};
