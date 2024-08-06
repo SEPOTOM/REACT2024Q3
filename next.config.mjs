4ede1e86708a5acace353b0d0eda19492007e100
Artyom Pizin <artempetr2016@gmail.com>
2024-08-06 15:43:57 +0300

    chore: add product images path to Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,16 @@ const nextConfig = {
       },
     ];
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.dummyjson.com',
+        port: '',
+        pathname: '/products/images/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
