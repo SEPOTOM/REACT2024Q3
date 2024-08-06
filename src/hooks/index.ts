ef748bf2ee4eacda5b2c510b1c059cde52767771
Artyom Pizin <artempetr2016@gmail.com>
2024-08-06 16:56:49 +0300

    refactor: delete useProducts hook as unnecessary

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,12 +1,10 @@
 import useSearchQuery from '@/hooks/useSearchQuery';
-import useProducts from '@/hooks/useProducts';
 import useCurrentPage from '@/hooks/useCurrentPage';
 import useIsPageLoading from '@/hooks/useIsPageLoading';
 import useCurrentSearchPageUrl from '@/hooks/useCurrentSearchPageUrl';
 
 export {
   useSearchQuery,
-  useProducts,
   useCurrentPage,
   useIsPageLoading,
   useCurrentSearchPageUrl,
