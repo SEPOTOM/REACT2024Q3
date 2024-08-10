bba73fe02c3d94970ff2da9fdffe1efa7e474979
Artyom Pizin <artempetr2016@gmail.com>
2024-08-10 19:57:15 +0300

    fix: fix displaying loading state for search route

--- app/search/[pageNumber]/loading.tsx
+++ app/search/[pageNumber]/loading.tsx
@@ -1,7 +1,18 @@
-import { StatusMessage } from '@/components';
+import { SearchHeader, StatusMessage } from '@/components';
+
+import styles from '@views/MainPage/MainPage.module.css';
 
 const SearchLoading = () => {
-  return <StatusMessage>Loading...</StatusMessage>;
+  return (
+    <div className={styles.mainPageColumn}>
+      <SearchHeader />
+      <main className={styles.main}>
+        <div className={`container ${styles.mainInner}`}>
+          <StatusMessage>Loading...</StatusMessage>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default SearchLoading;
