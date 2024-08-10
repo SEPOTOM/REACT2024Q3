8fce02aa3650477c5d65a1c59e954b182b7c480f
Artyom Pizin <artempetr2016@gmail.com>
2024-08-10 18:50:38 +0300

    feat(ProductPage): remove logic related to loading state

--- src/views/ProductPage/ProductPage.tsx
+++ src/views/ProductPage/ProductPage.tsx
@@ -5,9 +5,7 @@ import Image from 'next/image';
 
 import { useTheme } from '@/contexts';
 
-import { useCurrentSearchPageUrl, useIsPageLoading } from '@/hooks';
-
-import { StatusMessage } from '@/components';
+import { useCurrentSearchPageUrl } from '@/hooks';
 
 import { ProductPageProps } from '@views/ProductPage/types';
 
@@ -15,53 +13,39 @@ import styles from '@views/ProductPage/ProductPage.module.css';
 
 const ProductPage = ({ detailedProduct }: ProductPageProps) => {
   const theme = useTheme();
-  const { isDetailsPageLoading, isSearchPageLoading } = useIsPageLoading();
   const currentSearchPageUrl = useCurrentSearchPageUrl();
 
-  return isSearchPageLoading ? null : (
-      <div
-        className={`${styles.productPage} ${styles[`productPage_theme_${theme}`]}`}
-      >
-        <div className={styles.productPageContent}>
-          {isDetailsPageLoading ?
-            <StatusMessage>Loading...</StatusMessage>
-          : <>
-              <h2 className={styles.productPageTitle}>
-                {detailedProduct.title}
-              </h2>
-              {detailedProduct.images[0] && (
-                <Image
-                  src={detailedProduct.images[0]}
-                  alt={detailedProduct.title}
-                  width={500}
-                  height={500}
-                  className={styles.productPageImage}
-                />
-              )}
-              <p className={styles.productPageDescription}>
-                {detailedProduct.description}
-              </p>
-              <p className={styles.productPageFeature}>
-                Category: {detailedProduct.category}
-              </p>
-              <p className={styles.productPageFeature}>
-                Price: ${detailedProduct.price}
-              </p>
-              <Link
-                href={currentSearchPageUrl}
-                className={styles.productPageButton}
-              >
-                Close
-              </Link>
-            </>
-          }
-        </div>
-        <Link
-          href={currentSearchPageUrl}
-          className={styles.productPageShadow}
-        />
+  return (
+    <div
+      className={`${styles.productPage} ${styles[`productPage_theme_${theme}`]}`}
+    >
+      <div className={styles.productPageContent}>
+        <h2 className={styles.productPageTitle}>{detailedProduct.title}</h2>
+        {detailedProduct.images[0] && (
+          <Image
+            src={detailedProduct.images[0]}
+            alt={detailedProduct.title}
+            width={500}
+            height={500}
+            className={styles.productPageImage}
+          />
+        )}
+        <p className={styles.productPageDescription}>
+          {detailedProduct.description}
+        </p>
+        <p className={styles.productPageFeature}>
+          Category: {detailedProduct.category}
+        </p>
+        <p className={styles.productPageFeature}>
+          Price: ${detailedProduct.price}
+        </p>
+        <Link href={currentSearchPageUrl} className={styles.productPageButton}>
+          Close
+        </Link>
       </div>
-    );
+      <Link href={currentSearchPageUrl} className={styles.productPageShadow} />
+    </div>
+  );
 };
 
 export default ProductPage;
