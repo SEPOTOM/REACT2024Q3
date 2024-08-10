78ba7f0f74e893690576a9124f22888d0427527e
Artyom Pizin <artempetr2016@gmail.com>
2024-08-10 17:19:00 +0300

    feat(MainPage): remove logic related to loading state

--- src/views/MainPage/MainPage.tsx
+++ src/views/MainPage/MainPage.tsx
@@ -1,24 +1,17 @@
 'use client';
 
-import Link from 'next/link';
-
 import {
   ErrorButton,
   Pagination,
   ProductsFlyout,
   ProductsList,
   SearchForm,
-  StatusMessage,
   ThemesComboBox,
 } from '@/components';
 
 import { useTheme } from '@/contexts';
 
-import {
-  useCurrentSearchPageUrl,
-  useIsPageLoading,
-  useSearchQuery,
-} from '@/hooks';
+import { useSearchQuery } from '@/hooks';
 
 import { MainPageProps } from '@views/MainPage/types';
 
@@ -31,13 +24,10 @@ const MainPage = ({
 }: MainPageProps) => {
   const [searchQuery, setSearchQuery] = useSearchQuery();
   const theme = useTheme();
-  const currentSearchPageUrl = useCurrentSearchPageUrl();
 
   const handleSearchFormSubmit = (newSearchQuery: string): void => {
     setSearchQuery(newSearchQuery);
   };
-
-  const { isSearchPageLoading, isDetailsPageLoading } = useIsPageLoading();
 
   return (
     <div className={`${styles.mainPage} ${styles[`mainPage_theme_${theme}`]}`}>
@@ -54,28 +44,13 @@ const MainPage = ({
         </header>
         <main className={styles.main}>
           <div className={`container ${styles.mainInner}`}>
-            {isSearchPageLoading ?
-              <StatusMessage>Loading...</StatusMessage>
-            : <>
-                <ProductsList products={productsResponse.products} />
-                <Pagination totalPages={totalPages} />
-                <ProductsFlyout />
-              </>
-            }
+            <ProductsList products={productsResponse.products} />
+            <Pagination totalPages={totalPages} />
+            <ProductsFlyout />
           </div>
         </main>
       </div>
-      {children && !isSearchPageLoading && (
-        <div className={styles.mainPageColumn}>{children}</div>
-      )}
-      {isDetailsPageLoading && (
-        <div className={styles.mainPageColumn}>
-          <div className={styles.mainPageLoader}>
-            <StatusMessage>Loading...</StatusMessage>
-          </div>
-          <Link href={currentSearchPageUrl} className={styles.mainPageShadow} />
-        </div>
-      )}
+      {children && <div className={styles.mainPageColumn}>{children}</div>}
     </div>
   );
 };
