8253f06084c597ab3e9e524c3050cb78bb009bfb
Artyom Pizin <artempetr2016@gmail.com>
2024-08-10 17:16:23 +0300

    refactor: delete useIsPageLoading hooks as unnecessary

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,12 +2,6 @@
 
 import useSearchQuery from '@/hooks/useSearchQuery';
 import useCurrentPage from '@/hooks/useCurrentPage';
-import useIsPageLoading from '@/hooks/useIsPageLoading';
 import useCurrentSearchPageUrl from '@/hooks/useCurrentSearchPageUrl';
 
-export {
-  useSearchQuery,
-  useCurrentPage,
-  useIsPageLoading,
-  useCurrentSearchPageUrl,
-};
+export { useSearchQuery, useCurrentPage, useCurrentSearchPageUrl };
