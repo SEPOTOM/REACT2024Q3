a4e31912a7d69bfcd4831c323bb3b3474bd17dba
Artyom Pizin <artempetr2016@gmail.com>
2024-08-10 19:41:20 +0300

    feat(SearchLayout): remove header and leave minimal wrapper

--- src/views/SearchLayout/SearchLayout.tsx
+++ src/views/SearchLayout/SearchLayout.tsx
@@ -3,37 +3,15 @@
 import { ReactNode } from 'react';
 
 import { useTheme } from '@/contexts';
-import { useSearchQuery } from '@/hooks';
-
-import { ErrorButton, SearchForm, ThemesComboBox } from '@/components';
 
 import styles from '@views/MainPage/MainPage.module.css';
 
 const SearchLayout = ({ children }: { children: ReactNode }) => {
-  const [searchQuery, setSearchQuery] = useSearchQuery();
   const theme = useTheme();
-
-  const handleSearchFormSubmit = (newSearchQuery: string): void => {
-    setSearchQuery(newSearchQuery);
-  };
 
   return (
     <div className={`${styles.mainPage} ${styles[`mainPage_theme_${theme}`]}`}>
-      <div className={styles.mainPageColumn}>
-        <header className={styles.header}>
-          <div className={`container ${styles.headerInner}`}>
-            <ErrorButton />
-            <SearchForm
-              initialSearchQuery={searchQuery}
-              onFormSubmit={handleSearchFormSubmit}
-            />
-            <ThemesComboBox />
-          </div>
-        </header>
-        <main className={styles.main}>
-          <div className={`container ${styles.mainInner}`}>{children}</div>
-        </main>
-      </div>
+      {children}
     </div>
   );
 };
