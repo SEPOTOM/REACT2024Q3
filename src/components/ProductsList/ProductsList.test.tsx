554870c67c4684c0beba9d63529ca9fec32083b3
Artyom Pizin <artempetr2016@gmail.com>
2024-08-11 13:50:16 +0300

    test(ProductsList): fix tests to work with Next.js App Router

--- src/components/ProductsList/ProductsList.test.tsx
+++ src/components/ProductsList/ProductsList.test.tsx
@@ -1,34 +1,10 @@
-import { Mock } from 'vitest';
 import { renderWithUser } from '@tests/utils';
-import { useRouter } from 'next/router';
 
 import { createFakeProducts } from '@tests/mocks/products';
-import { createFakeRouter } from '@tests/mocks/router';
 
 import { ProductsList } from '@/components';
 
-beforeAll(() => {
-  (useRouter as Mock).mockReturnValue(
-    createFakeRouter({
-      pathname: '/search/2',
-      query: { pageNumber: '2' },
-      asPath: '/search/2',
-      route: '/search/[pageNumber]',
-    }),
-  );
-});
-
-afterAll(() => {
-  vi.resetAllMocks();
-});
-
 test('ProductsList renders the specified number of cards', () => {
-  (useRouter as Mock).mockReturnValueOnce({
-    pathname: '/search/2',
-    query: { pageNumber: '2' },
-    asPath: '/search/2',
-    route: '/search/[pageNumber]',
-  });
   const fakeProducts = createFakeProducts(3);
 
   const { getAllByRole } = renderWithUser(
