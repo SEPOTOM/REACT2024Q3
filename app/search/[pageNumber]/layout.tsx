5579ffd168cf6600a3204c12f0c4d24928939df3
Artyom Pizin <artempetr2016@gmail.com>
2024-08-11 13:02:09 +0300

    feat: remove layout for search route

--- app/search/[pageNumber]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-import { SearchLayout } from '@/views';
-
-const Layout = ({ children }: { children: ReactNode }) => {
-  return <SearchLayout>{children}</SearchLayout>;
-};
-
-export default Layout;
