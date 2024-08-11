727dcfe4b93b2a9c29611c17f370b710b423b573
Artyom Pizin <artempetr2016@gmail.com>
2024-08-11 13:02:49 +0300

    refactor: delete SearchLayout component as unnecessary

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,7 +1,6 @@
 import MainPage from '@views/MainPage/MainPage';
 import NotFoundPage from '@views/NotFoundPage/NotFoundPage';
 import ProductPage from '@views/ProductPage/ProductPage';
-import SearchLayout from '@/views/SearchLayout/SearchLayout';
 import SearchPageColumns from '@views/SearchPageColumns/SearchPageColumns';
 
-export { MainPage, NotFoundPage, ProductPage, SearchLayout, SearchPageColumns };
+export { MainPage, NotFoundPage, ProductPage, SearchPageColumns };
