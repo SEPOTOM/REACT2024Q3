1dae29e294a0993104f966d548ed9b9c634f343f
Artyom Pizin <artempetr2016@gmail.com>
2024-08-11 13:19:03 +0300

    feat(SearchPage): remove children prop

--- src/views/SearchPage/types.ts
+++ src/views/SearchPage/types.ts
@@ -1,9 +1,6 @@
-import { ReactNode } from 'react';
-
 import { SearchProductsResponse } from '@services/types';
 
 export interface SearchPageProps {
-  children?: ReactNode;
   totalPages: number;
   productsResponse: SearchProductsResponse;
 }
