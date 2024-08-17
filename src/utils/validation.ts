f3f9885211dcb36418fe38c0832277519b042bdc
Artyom Pizin <artempetr2016@gmail.com>
2024-08-17 20:00:43 +0300

    feat(validation): add age field to the yup schema

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -5,6 +5,12 @@ export const schema = yup.object({
     .string()
     .required('Username is required')
     .matches(/^[A-Z]/, 'Username must start with an uppercase letter'),
+  age: yup
+    .number()
+    .typeError('Age must be a number')
+    .required('Age is required')
+    .positive('Age must be a positive number')
+    .integer('Age must be an integer'),
 });
 
 export type FormData = yup.InferType<typeof schema>;
