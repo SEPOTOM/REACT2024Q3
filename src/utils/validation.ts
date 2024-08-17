6f1de80112381c8b942f9cd43eebf9d4da7db66b
Artyom Pizin <artempetr2016@gmail.com>
2024-08-17 19:54:03 +0300

    feat(validation): add username filed to the yup schema

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,5 +1,10 @@
 import * as yup from 'yup';
 
-export const schema = yup.object({});
+export const schema = yup.object({
+  username: yup
+    .string()
+    .required('Username is required')
+    .matches(/^[A-Z]/, 'Username must start with an uppercase letter'),
+});
 
 export type FormData = yup.InferType<typeof schema>;
