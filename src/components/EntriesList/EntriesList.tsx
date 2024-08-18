a2a1cb089c0f5fbb92635d4c094eb432ccf93ae2
Artyom Pizin <artempetr2016@gmail.com>
2024-08-18 18:23:45 +0300

    feat(EntriesList): add styles

--- src/components/EntriesList/EntriesList.tsx
+++ src/components/EntriesList/EntriesList.tsx
@@ -2,9 +2,11 @@ import { EntryCard } from '@/components';
 
 import { EntriesListProps } from '@components/EntriesList/types';
 
+import styles from '@components/EntriesList/EntriesList.module.css';
+
 const EntriesList = ({ entries }: EntriesListProps) => {
   return (
-    <ul>
+    <ul className={styles.list}>
       {entries.map((entry, index) => (
         <li key={index}>
           <EntryCard entry={entry} />
