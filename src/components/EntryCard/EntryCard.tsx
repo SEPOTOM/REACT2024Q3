5548ce2b79894cd8e66d2b148727f33bc50d5043
Artyom Pizin <artempetr2016@gmail.com>
2024-08-18 18:24:07 +0300

    feat(EntryCard): add styles

--- src/components/EntryCard/EntryCard.tsx
+++ src/components/EntryCard/EntryCard.tsx
@@ -1,5 +1,7 @@
 import { EntryCardProps } from '@components/EntryCard/types';
 
+import styles from '@components/EntryCard/EntryCard.module.css';
+
 const EntryCard = ({ entry }: EntryCardProps) => {
   const {
     username,
@@ -14,17 +16,40 @@ const EntryCard = ({ entry }: EntryCardProps) => {
   } = entry;
 
   return (
-    <div>
-      <p>Username: {username}</p>
-      <p>Age: {age}</p>
-      <p>Email: {email}</p>
-      <p>Password: {password}</p>
-      <p>Confirm password: {confirmPassword}</p>
-      <p>Gender: {gender}</p>
-      <p>Is T&C accepted: {isTermsAccepted ? 'Yes' : 'No'}</p>
-      <p>Country: {country}</p>
-      <p>
-        Uploaded image: <img src={picture} alt="Uploaded image" />
+    <div className={styles.card}>
+      <div className={styles.cardColumn}>
+        <p className={styles.text}>
+          <span className={styles.label}>Username:</span> {username}
+        </p>
+        <p className={styles.text}>
+          <span className={styles.label}>Age:</span> {age}
+        </p>
+        <p className={styles.text}>
+          <span className={styles.label}>Email:</span> {email}
+        </p>
+        <p className={styles.text}>
+          <span className={styles.label}>Password:</span> {password}
+        </p>
+        <p className={styles.text}>
+          <span className={styles.label}>Confirm password:</span>{' '}
+          {confirmPassword}
+        </p>
+        <p className={styles.text}>
+          <span className={styles.label}>Gender:</span> {gender}
+        </p>
+        <p className={styles.text}>
+          <span className={styles.label}>Is T&C accepted:</span>{' '}
+          {isTermsAccepted ? 'Yes' : 'No'}
+        </p>
+        <p className={styles.text}>
+          <span className={styles.label}>Country:</span> {country}
+        </p>
+      </div>
+      <p className={`${styles.cardColumn} ${styles.text}`}>
+        <span className={styles.label}>Uploaded image:</span>{' '}
+        <div className={styles.picture}>
+          <img src={picture} alt="Uploaded image" className={styles.image} />
+        </div>
       </p>
     </div>
   );
