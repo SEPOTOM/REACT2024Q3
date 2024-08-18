ebf064cfaa205626f2a8b64599cd77ccf6b3e32c
Artyom Pizin <artempetr2016@gmail.com>
2024-08-18 13:43:37 +0300

    feat(MainPage): add links to controlled and uncontrolled form routes

--- src/views/MainPage/MainPage.tsx
+++ src/views/MainPage/MainPage.tsx
@@ -1,5 +1,22 @@
+import { Link } from 'react-router-dom';
+
 const MainPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/controlled">Controlled Form</Link>
+            </li>
+            <li>
+              <Link to="/uncontrolled">Uncontrolled Form</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 };
 
 export default MainPage;
